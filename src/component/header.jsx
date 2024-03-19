4c10405c24d331dd195ea7f1577a4146606e31a2
ksvaska <vas25ks@gmail.com>
2024-03-19 17:35:38 +0300

    first

---
+++ src/component/header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    return(
+        <div className="header">
+            <h1>Изучай языки вместе с &#10078;CatLexicon&#10078;</h1>
+            <p>Добавляй новые слова и тренируй их &#11015;</p>
+        </div>
+    );
+}
+
+export default Header
